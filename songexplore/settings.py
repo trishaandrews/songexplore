6881428df4ebcb7f863d81f6a1b43dc3a96ece60
Trisha Andrews <andrews.trisha.a@gmail.com>
2015-09-16 20:37:09 -0400

    fix params

--- songexplore/settings.py
+++ songexplore/settings.py
@@ -4,4 +4,4 @@ with open("params.csv", 'r') as pf:
     for line in pf:
         kvs = line.strip().split(",")
         vals[kvs[0]] = kvs[1]       
-params = (vals["user"], vals["host"], vals["database"])
+params = (vals["user"], vals["password"], vals["host"], vals["database"])
