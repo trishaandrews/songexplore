8f89ad3005e65a5ca556202f97d3dae38a41cd73
Trisha Andrews <andrews.trisha.a@gmail.com>
2015-09-16 20:20:24 -0400

    update css mostly

--- songexplore/static/recommendations.js
+++ songexplore/static/recommendations.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
     var selected_items = {"items":[], "page":0, "orderby":"track_id", "asc":"asc"};
     
     function entry_ajax() {
-	    $.ajax({
+        $.ajax({
             url: './add_entry',
             data: selected_items,
-	    type: 'POST',
+            type: 'POST',
             success: function (data) {
                 json_data = JSON.parse(data);
                 recs = "Recommendations:<br> <ul>";
@@ -17,7 +17,7 @@ $(document).ready(function() {
                 console.log("error! "+error);
             }
         });
-	}
+    }
     function box_content(data_list, words){
         for(var i=0; i<data_list.length; i++){
             display_words = "";
@@ -36,37 +36,37 @@ $(document).ready(function() {
     }
     NoResults = "No Results"; //TODO No Results message for empty autocomplete
     $('#autocomplete').autocomplete({
-	source: function (request, response) {
+        source: function (request, response) {
             $.getJSON('./autocomplete',{ //'{{ url_for("autocomplete") }}',{
-		search: request,
+                search: request,
             }, response);
         },
-	minLength: 1,                      
+        minLength: 1,                      
     });
     $("#autocomplete").on("autocompleteselect", function(event, ui) {
-	selected_labels.push(ui.item.value);
-	selected_items["items"].push(ui.item);
-	selected_items["page"] = 0;
-	//console.log(ui.item.cluster, ui.item.cluster_new);
-	entry_ajax();
-	var output = "Chosen songs:<br> <ul>"
-	output = box_content(selected_labels, output);
-	$('#outputcontent').html(output);
-	$(this).val(''); return false; 
+        selected_labels.push(ui.item.value);
+        selected_items["items"].push(ui.item);
+        selected_items["page"] = 0;
+        //console.log(ui.item.cluster, ui.item.cluster_new);
+        entry_ajax();
+        var output = "Chosen songs:<br> <ul>"
+        output = box_content(selected_labels, output);
+        $('#outputcontent').html(output);
+        $(this).val(''); return false; 
     });
     $('#more').on('click', function (event) {
         selected_items["page"] += 1;
         entry_ajax();
     });
     $("input[name=orderby]:radio").change(function () {
-	    selected_items["orderby"] = this.value;
-	    selected_items["page"] = 0;
-	    entry_ajax();
-	});
-	$("input[name=ascdesc]:radio").change(function () {
-	    selected_items["asc"] = this.value;
-	    selected_items["page"] = 0;
-	    entry_ajax();
-	});
-	
+        selected_items["orderby"] = this.value;
+        selected_items["page"] = 0;
+        entry_ajax();
+    });
+    $("input[name=ascdesc]:radio").change(function () {
+        selected_items["asc"] = this.value;
+        selected_items["page"] = 0;
+        entry_ajax();
+    });
+    
 });
